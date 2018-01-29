f2da2395d83715f88a2f686e74466f2d9f5df087
salihbout <salihb@qq.com>
2018-01-30 00:45:23 +0800

    Chat UI

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -1,27 +1,44 @@
 <template>
-  <el-container style="height: 1080px; border: 1px solid #eee">
-    <el-aside width="300px">
+  <el-row :gutter="20"  class="ChatWrapper">
+      
+    <el-col :span="3">
             <el-card class="box-card">
-                <div slot="header" class="clearfix OnlineUsersHeader">
+                <div slot="header" class="clearfix">
                     <span>Online Users (8)</span>
                 </div>
-                <el-row v-for="o in 8" :key="o" class="UserOnline">
-                    <el-col :span="6"><img  src="https://static.pexels.com/users/avatars/7344/stefan-stefancik-817-medium.jpeg" alt="avatar"></el-col>
-                    <el-col :span="18"><div class="UserOnlineInfo " >
+                <el-row :gutter="20" v-for="o in 10" :key="o" class="UserOnline">
+                    <el-col :span="6"><img class="ChatAvatar" src="https://static.pexels.com/users/avatars/7344/stefan-stefancik-817-medium.jpeg" alt="avatar"></el-col>
+                    <el-col :span="18">
                         <h5>{{'Username ' + o }}</h5>
-                        <small>hello</small>
-                    </div>
                     </el-col>
                     
                 </el-row>
             </el-card>
-    </el-aside>
+    </el-col>
     
-    <el-container>
-            
+    <el-col :span="21" class="MessagesWrapper">
+            <el-card class="messages">
+               
+                    <el-row v-for="o in 4" :key="o">
+                   
+                   <el-col :span="1"><img class="ChatAvatar" src="https://static.pexels.com/users/avatars/7344/stefan-stefancik-817-medium.jpeg" alt="avatar"></el-col>
+                    <el-col :span="23">
+                        <h5>{{'Username ' + o }}</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod earum et accusamus labore maxime nobis iusto vero recusandae, architecto minima dolores ducimus autem dolore sequi facilis perferendis, debitis ea facere.</p>
+                    </el-col>
+                   
+                </el-row>
+                
+                
+                 <el-form :model="MessageForm" :rules="rules" ref="ruleForm"  class="demo-ruleForm">
+                        <el-form-item prop="desc">
+                            <el-input type="textarea" v-model="message" class="MessageInput"></el-input>
+                        </el-form-item>
+                    </el-form>
+            </el-card>
 
-    </el-container>
-</el-container>
+    </el-col>
+</el-row>
 </template>
 
 
@@ -35,17 +52,51 @@ export default {
 <style scoped>
 
 
-.UserOnline{
+.ChatWrapper {
+    margin-top: 15px;
 
-    padding:2px;
-    border-bottom:1px solid #ddd;
-    margin:0;
+
+}
+.UserOnline{
+    width: 100%;
+    margin-bottom: 2px;
+    padding-bottom: 10px;
 }
 
-.UserOnline img {
-    height: 64px;
-    width: 64px;
+.UserOnline h5{
+    margin-left: 10px;
+}
+
+.ChatAvatar {
+    height: 40px;
+    width: 40px;
+    border-radius: 50px;
+    margin-top: 10px;
+    border: 3px solid rgb(19, 107, 221);
     
+}
+
+.MessagesWrapper {
+    overflow: auto;
+    max-height: 100vh; 
+}
+
+.messages {
+    height: 850px;
+}
+
+.MessageInput {
+    position: fixed;
+    bottom: 0px;
+    width: 100%;
+    height: 50px;
+    z-index: 99;
+    background: #fafafa;
+    border: none;
+    outline: none;
+
+    color: #666;
+    font-weight: 400;
 }
 
 </style>
