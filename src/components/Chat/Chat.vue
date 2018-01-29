1f50a5de966c05ce08d25b96eaa13477d7853dc0
salihbout <salihb@qq.com>
2018-01-29 21:43:11 +0800

    Chat layout update

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -1,7 +1,27 @@
 <template>
-  <div> 
-      <p>Welcome to chat !</p>
-  </div>
+  <el-container style="height: 1080px; border: 1px solid #eee">
+    <el-aside width="300px">
+            <el-card class="box-card">
+                <div slot="header" class="clearfix OnlineUsersHeader">
+                    <span>Online Users (8)</span>
+                </div>
+                <el-row v-for="o in 8" :key="o" class="UserOnline">
+                    <el-col :span="6"><img  src="https://static.pexels.com/users/avatars/7344/stefan-stefancik-817-medium.jpeg" alt="avatar"></el-col>
+                    <el-col :span="18"><div class="UserOnlineInfo " >
+                        <h5>{{'Username ' + o }}</h5>
+                        <small>hello</small>
+                    </div>
+                    </el-col>
+                    
+                </el-row>
+            </el-card>
+    </el-aside>
+    
+    <el-container>
+            
+
+    </el-container>
+</el-container>
 </template>
 
 
@@ -13,5 +33,19 @@ export default {
 
 
 <style scoped>
+
+
+.UserOnline{
+
+    padding:2px;
+    border-bottom:1px solid #ddd;
+    margin:0;
+}
+
+.UserOnline img {
+    height: 64px;
+    width: 64px;
+    
+}
 
 </style>
